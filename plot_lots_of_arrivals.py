cad6e85a89e274256fc455ba0f7acb98f5785f22
jsimpsonUoA <jsim921@aucklanduni.ac.nz>
2019-03-14 15:36:22 +1300

    Delete plot_lots_of_arrivals.py

--- plot_lots_of_arrivals.py
+++
@@ -1,6 +0,0 @@
-import plotting as pl
-
-main_dir = '/home/jonathan/Dropbox/timber_work/Data/sg6_90_rot-1/p_wave_picks.csv'
-dirs = [main_dir+'-'+str(i) for i in range(36)]
-
-pl.arrival_times_plot(dirs, save_dir=None, pick_errors=None, polar=False,picks_offset=0., labels=list(range(87)), tick_fontsize=14.0, figsize=(6,6),legend_fontsize=1.)
